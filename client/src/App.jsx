bb8c9a9ef22280391b00451d2ce5f9513383bbe6
DikshakAdhikari <ddikshakk@gmail.com>
2023-09-05 01:58:52 +0530

    filter left

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from 'react'
 import './App.css'
 import axios from 'axios';
 import { base_url } from './config/location';
+import { Search } from './componentss/Search';
+import { Table } from './componentss/Table';
 
 function App() {
   const [obj, setobj]= useState()
@@ -27,9 +29,27 @@ function App() {
   },[sort,filterGenre,page,search]);
 
   return (
-    <>
-     
-    </>
+    <div className='w-[100%] min-h-screen flex items-center justify-center '>
+      <div className='w-[1000px] border-r-[10px] shadow-slate-400 flex flex-col overflow-hidden bg-white '>
+        <div className='flex content-center h-[80px] bg-black justify-between'>
+          <img src="../public/images/logo.png" alt="logo" className='w-[100px] object-contain ml-[15px]' />
+          <Search setSearch={(search)=> setSearch(search)} />
+        </div>
+        <div className='h-[500px] flex'>
+          <div className='flex-3 relative'>
+            <Table movies={obj.movies?obj.movies:[]} />
+          </div>
+            <div className='flex-1'> </div>
+
+            
+
+          
+
+        </div>
+
+      </div>
+
+    </div>
   )
 }
 
---
+++ client/src/componentss/Search.jsx
@@ -0,0 +1,11 @@
+
+export const Search = ({setSearch})=> {
+    return (
+        <input 
+        type="text"
+         placeholder="Search"
+          className="mr-[15px] h-[40px] self-center outline-none border-none w-[400px] rounded-[4px] pl-[5px] text-[15px] "
+           onChange={({currentTarget: input})=> setSearch(input.value)} 
+            />
+    )
+}
